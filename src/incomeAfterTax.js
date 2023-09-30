a11b0f31fa15021e99c925ca0d9fba368548b8b3
Peter Vacouftsis <reteppetergcz@gmail.com>
2023-10-01 01:39:58 +1000

    Updated styling and added more frequency options

--- src/incomeAfterTax.js
+++ src/incomeAfterTax.js
@@ -4,32 +4,50 @@ const { default: taxBrackets } = await import("./taxBrackets.json", { assert: { 
 main();
 
 async function main(){
-	let isPerWeek = document.getElementById("isPerWeek");
+	let payPeriodSelector = document.getElementById("payPeriodSelector");
 	let includeMyki = document.getElementById("includeMyki");
 	let isMykiPrepaid = document.getElementById("isMykiPrepaid");
-	let wantedIncomeSetter = document.getElementById("wantedIncomeSetter");
+	let incomeSetter = document.getElementById("incomeSetter");
+	let incomeSetterSaver = document.getElementById("incomeSetterSaver");
+	let beforeOrAfterTax = document.getElementById("beforeOrAfterTax");
+
+	let ContainerMykiStuff = document.getElementById("ContainerMykiStuff");
+	let ContainerAddTax = document.getElementById("ContainerAddTax");
+	let ContainerRemoveTax = document.getElementById("ContainerRemoveTax");
 	
 	let hourlyWageOutput = document.getElementById("hourlyWageOutput");
 	let dailyWageOutput = document.getElementById("dailyWageOutput");
 	let weeklyWageOutput = document.getElementById("weeklyWageOutput");
 	let yearlyWageOutput = document.getElementById("yearlyWageOutput");
+	let yearlyTaxOutput = document.getElementById("yearlyTaxOutput");
+	let yearlyMedicareOutput = document.getElementById("yearlyMedicareOutput");
 
-	isPerWeek.addEventListener("change", (event) => { redraw(); });
+	let ThenField = document.getElementById("ThenField");
+
+	payPeriodSelector.addEventListener("change", (event) => { redraw(); });
 	includeMyki.addEventListener("change", (event) => { redraw(); });
 	isMykiPrepaid.addEventListener("change", (event) => { redraw(); });
-	wantedIncomeSetter.addEventListener("change", (event) => { redraw(); });
-	
+	incomeSetter.addEventListener("change", (event) => { redraw(); });
 
-	let isPerWeek2 = document.getElementById("isPerWeek2");
-	let salarySetter = document.getElementById("salarySetter");
+	//incomeSetter.addEventListener("focus", (event) => { incomeSetter.value = incomeSetterSaver.value; });
 
-	let hourlyWageOutput2 = document.getElementById("hourlyWageOutput2");
-	let dailyWageOutput2 = document.getElementById("dailyWageOutput2");
-	let weeklyWageOutput2 = document.getElementById("weeklyWageOutput2");
-	let yearlyWageOutput2 = document.getElementById("yearlyWageOutput2");
-	
-	isPerWeek2.addEventListener("change", (event) => { redraw(); });
-	salarySetter.addEventListener("change", (event) => { redraw(); });
+	let isBefore = false;
+
+	let updateVisibilityBeforeOrAfterTax = function(){
+		let choice = beforeOrAfterTax.value;
+		isBefore = choice === "before";
+
+		// ContainerAddTax.hidden = isBefore;
+		// ContainerRemoveTax.hidden = !isBefore;
+		ContainerMykiStuff.hidden = isBefore;
+	};
+
+	updateVisibilityBeforeOrAfterTax();
+
+	beforeOrAfterTax.addEventListener("change", (event) => {
+		updateVisibilityBeforeOrAfterTax();
+		redraw(); 
+	});
 
 	const delay = ms => new Promise(res => setTimeout(res, ms));
 	
@@ -41,89 +59,95 @@ async function main(){
 		bracket.equivalentMin = previousBracket.equivalentMin + (bracket.min - previousBracket.min) * (1 - previousBracket.percent - medicareLevy.value);
 		bracket.alreadyPaidTax = previousBracket.alreadyPaidTax + (bracket.min - previousBracket.min) * previousBracket.percent;
 		previousBracket = bracket;
-		console.log(bracket);
 	}
 	
 	let redraw = async function(){
-		//Determine visibilities
-		{
-			isMykiPrepaid.hidden = !includeMyki.checked;
-		}
-		
+		const monthsPerYear = 12;
 		const weeksPerYear = 52;
 		const hoursPerDay = 8;
 		const daysPerWeek = 5;
-		const daysPerYear = 52;
+		const daysPerYear = weeksPerYear * daysPerWeek;
+		const hoursPerYear = daysPerYear * hoursPerDay;
 		const yearlyToWeekly = 1 / weeksPerYear;
 		const yearlyToDaily = 1 / daysPerWeek / weeksPerYear;
 		const yearlyToHourly = 1 / daysPerWeek / weeksPerYear / hoursPerDay;
-		
-		let wantedIncome = Number(wantedIncomeSetter.value);
 
-		let totalIncome = wantedIncome;
+		let income = Number(incomeSetter.value);
+		incomeSetterSaver.value = income;
 
-		if(isPerWeek.checked){
-			// const hoursPerDay = 8;
-			// const daysPerWeek = 5;
-			// const hoursPerWeek = hoursPerDay * daysPerWeek;
-			// totalIncome	= wantedIncome * hoursPerWeek;
-			
-			const weeksPerYear = 52;
-			totalIncome = wantedIncome * weeksPerYear;
+		if(payPeriodSelector.value === "hourly"){
+			income = income * hoursPerYear;
+		}else if(payPeriodSelector.value === "daily"){
+			income = income * daysPerYear;
+		}else if(payPeriodSelector.value === "weekly"){
+			income = income * weeksPerYear;
+		}else if(payPeriodSelector.value === "monthly"){
+			income = income * monthsPerYear;
 		}
-		
-		if(includeMyki.checked){
-			if(isMykiPrepaid.checked){
-				totalIncome += 6 * 325;
-			} else{
-				totalIncome += 10 * (365 * 5.0/7.0);
+
+		//incomeSetter.value = moneyFormat.format(income);
+
+		if(!isBefore){
+			ThenField.innerHTML = "Then, before tax you have to make:";
+
+			//Determine visibilities
+			{
+				isMykiPrepaid.hidden = !includeMyki.checked;
 			}
-		}
-		
-		{
-			let previousBracket = {equivalentMin:0, min:0, percent:0};
-			for(let bracket of taxBrackets.brackets){
-				if(bracket.equivalentMin > totalIncome){ break; }
-				previousBracket = bracket;
-			}
-			
-			let neededYearlyWage = (totalIncome - previousBracket.equivalentMin) / (1 - previousBracket.percent - medicareLevy.value) + previousBracket.min;
-			
-			let medicareTax = neededYearlyWage * medicareLevy.value;
-			let incomeTax = neededYearlyWage - totalIncome - medicareTax;
-			
-			hourlyWageOutput.innerHTML = "Hourly: " + moneyFormat.format(neededYearlyWage * yearlyToHourly);
-			dailyWageOutput.innerHTML = "Daily: " + moneyFormat.format(neededYearlyWage * yearlyToDaily);
-			weeklyWageOutput.innerHTML = "Weekly: " + moneyFormat.format(neededYearlyWage * yearlyToWeekly);
-			yearlyWageOutput.innerHTML = "Yearly: " + moneyFormat.format(neededYearlyWage) + ", " + moneyFormat.format(incomeTax) + ", " + moneyFormat.format(medicareTax);
-		}
-		
-		
-		
-		let totalSalary = Number(salarySetter.value);
 
-		let finalSalary = totalSalary;
-
-		if(isPerWeek2.checked){
-			finalSalary = totalSalary * weeksPerYear;
-		}
-		
-		{
-			let previousBracket = {equivalentMin:0, min:0, percent:0};
-			for(let bracket of taxBrackets.brackets){
-				if(bracket.equivalentMin > finalSalary){ break; }
-				previousBracket = bracket;
+			let totalIncome = income;
+			
+			if(includeMyki.checked){
+				if(isMykiPrepaid.checked){
+					totalIncome += 6 * 325;
+				} else{
+					totalIncome += 10 * (365 * 5.0/7.0);
+				}
 			}
 			
-			let medicareTax = finalSalary * medicareLevy.value;
-			let incomeTax = previousBracket.alreadyPaidTax + (finalSalary - previousBracket.min) * previousBracket.percent;
+			{
+				let previousBracket = {equivalentMin:0, min:0, percent:0};
+				for(let bracket of taxBrackets.brackets){
+					if(bracket.equivalentMin > totalIncome){ break; }
+					previousBracket = bracket;
+				}
+				
+				let neededYearlyWage = (totalIncome - previousBracket.equivalentMin) / (1 - previousBracket.percent - medicareLevy.value) + previousBracket.min;
+				
+				let medicareTax = neededYearlyWage * medicareLevy.value;
+				let incomeTax = neededYearlyWage - totalIncome - medicareTax;
+				
+				hourlyWageOutput.innerHTML = "Hourly: " + moneyFormat.format(neededYearlyWage * yearlyToHourly);
+				dailyWageOutput.innerHTML = "Daily: " + moneyFormat.format(neededYearlyWage * yearlyToDaily);
+				weeklyWageOutput.innerHTML = "Weekly: " + moneyFormat.format(neededYearlyWage * yearlyToWeekly);
+				yearlyWageOutput.innerHTML = "Yearly: " + moneyFormat.format(neededYearlyWage);
+				yearlyTaxOutput.innerHTML = "Tax: " + moneyFormat.format(incomeTax);
+				yearlyMedicareOutput.innerHTML = "Medicare costs: " + moneyFormat.format(medicareTax);
+			}
+		} else{
+			ThenField.innerHTML = "Then, after tax you make:";
+
+			let finalSalary = income;
 			
-			let finalWage = finalSalary - incomeTax - medicareTax
-			
-			hourlyWageOutput2.innerHTML = "Hourly: " + moneyFormat.format(finalWage * yearlyToHourly);
-			dailyWageOutput2.innerHTML = "Daily: " + moneyFormat.format(finalWage * yearlyToDaily);
-			weeklyWageOutput2.innerHTML = "Weekly: " + moneyFormat.format(finalWage * yearlyToWeekly);
-			yearlyWageOutput2.innerHTML = "Yearly: " + moneyFormat.format(finalWage) + ", " + moneyFormat.format(incomeTax) + ", " + moneyFormat.format(medicareTax);
+			{
+				let previousBracket = {equivalentMin:0, min:0, percent:0};
+				for(let bracket of taxBrackets.brackets){
+					if(bracket.equivalentMin > finalSalary){ break; }
+					previousBracket = bracket;
+				}
+				
+				let medicareTax = finalSalary * medicareLevy.value;
+				let incomeTax = previousBracket.alreadyPaidTax + (finalSalary - previousBracket.min) * previousBracket.percent;
+				
+				let finalWage = finalSalary - incomeTax - medicareTax
+				
+				hourlyWageOutput.innerHTML = "Hourly: " + moneyFormat.format(finalWage * yearlyToHourly);
+				dailyWageOutput.innerHTML = "Daily: " + moneyFormat.format(finalWage * yearlyToDaily);
+				weeklyWageOutput.innerHTML = "Weekly: " + moneyFormat.format(finalWage * yearlyToWeekly);
+				yearlyWageOutput.innerHTML = "Yearly: " + moneyFormat.format(finalWage);
+				yearlyTaxOutput.innerHTML = "Tax: " + moneyFormat.format(incomeTax);
+				yearlyMedicareOutput.innerHTML = "Medicare costs: " + moneyFormat.format(medicareTax);
+			}
 		}
 	};
 
